fdfa8e22d64adf33350d733a01a6887558445b70
Patrick van Halm <pvanhalm00@gmail.com>
2021-10-07 11:58:20 +0200

    Add wires puzzle

---
+++ Assets/Scripts/Puzzles/Puzzle2D.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public enum PuzzleState
+{
+    NotStarted,
+    Started,
+    Finished
+}
+
+public abstract class Puzzle2D : MonoBehaviour, IRaycastable3D
+{
+    [Header("Puzzle2D Settings")]
+    public float interactionRange;
+    public GameObject canvas;
+
+    [Header("Puzzle2D Events")]
+    public UnityEvent OnPuzzleFinished;
+
+    public PuzzleState PuzzleState { get; private set; }
+    public bool IsOpen => canvas.activeSelf;
+
+    private PlayerController controller;
+
+    protected virtual void Start()
+    {
+        controller = FindObjectOfType<PlayerController>();
+    }
+
+    protected virtual void Toggle() 
+    {
+        controller.SetCCEnabled(false);
+        canvas.SetActive(!IsOpen);
+        if (IsOpen && PuzzleState == PuzzleState.NotStarted) PuzzleState = PuzzleState.Started;
+        if (IsOpen)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    protected void Finish()
+    {
+        PuzzleState = PuzzleState.Finished;
+        OnPuzzleFinished?.Invoke();
+    }
+
+    public virtual void OnRaycastStay(RaycastHit hit)
+    {
+        if (hit.distance > interactionRange) return;
+        if (!Input.GetKeyDown(KeyCode.E)) return;
+        Toggle();
+    }
+    public virtual void OnRaycastEnter(RaycastHit hit){}
+    public virtual void OnRaycastExit(){}
+}
---
+++ Assets/Scripts/UI/MovableImageRect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(RectTransform))]
+public class MovableImageRect : MonoBehaviour
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+    public float lineWidth = 10;
+
+    public RectTransform imageRectTransform;
+
+
+    public void UpdateTransform()
+    {
+        Vector3 differenceVector = pointB - pointA;
+        imageRectTransform.sizeDelta = new Vector2(differenceVector.magnitude, lineWidth);
+        imageRectTransform.pivot = new Vector2(0, 0.5f);
+        imageRectTransform.position = pointA;
+        float angle = Mathf.Atan2(differenceVector.y, differenceVector.x) * Mathf.Rad2Deg;
+        imageRectTransform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
