d1b317345f0740bba57e68bc74d5fcce78770928
Patrick van Halm <pvanhalm00@gmail.com>
2021-11-02 10:34:35 +0100

    Add interactable highlighting

--- Assets/Scripts/Extensions/ComponentExtensions.cs
+++ Assets/Scripts/Extensions/ComponentExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-static class ComponentExtensions
+public static class ComponentExtensions
 {
     public static T[] GetComponentsOnlyInChildren<T>(this Component comp) where T : Component
     {
